ffcec14142bb10a4a79ff62719a3f44744cc1d5c
Harsh Shastrakar <harshshastrakar011@gmail.com>
2024-01-23 15:58:36 +0530

    Similar

--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-import Carousel from "../../../components/carousel/Carousel";
+import Carousel from "../../../components/Carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Similar = ({ mediaType, id }) => {
   const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
 
   const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
-
+  //Add Carsilul
+  //Add Carsilul
   return (
     <Carousel
       title={title}
